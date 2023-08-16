23a8351754aa95ccdef75e6cfb7d34740439718c
WojcikArek <wojcikarek10@gmail.com>
2023-08-16 09:00:14 +0200

    add Devise to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :clients
   # get 'home/index'
   root 'home#index'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # get "client" => "clients#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
