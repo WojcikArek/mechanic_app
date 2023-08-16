573b7d7b8910722fd15ca7b197d14e8e7ba96544
WojcikArek <wojcikarek10@gmail.com>
2023-08-16 09:00:46 +0200

    Migration DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_20_175226) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_26_101916) do
   create_table "clients", force: :cascade do |t|
     t.string "First_name"
     t.string "Last_name"
     t.string "Phone"
     t.string "Car"
-    t.string "Model"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "Model"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
